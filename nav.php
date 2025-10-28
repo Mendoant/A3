9994eff96fd0ce06048960a71049c77ac562974d
Mendoant <89116354+Mendoant@users.noreply.github.com>
2025-10-28 17:39:33 -0400

    Add simple navigation to dashboard pages

---
+++ nav.php
@@ -0,0 +1,9 @@
+<?php
+// nav.php - simple navigation included on dashboard pages
+?>
+<nav class="topnav">
+    <a href="dashboard_scm.php">SCM Dashboard</a> |
+    <a href="dashboard_erp.php">ERP Dashboard</a> |
+    <a href="index.php">Home</a> |
+    <a href="logout.php">Logout</a>
+</nav>
