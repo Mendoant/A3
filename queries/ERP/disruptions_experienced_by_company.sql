ae956ef28d4ef46efc041dea1d68daee45fc48ce
eligonzalezz <elisamgoneli@gmail.com>
2025-11-13 13:57:46 -0500

    Update disruptions_experienced_by_company.sql

--- queries/ERP/disruptions_experienced_by_company.sql
+++ queries/ERP/disruptions_experienced_by_company.sql
@@ -1,15 +1,14 @@
--- Change ic.AffectedCompanyID, find a way for PHP to communicate this information directly to modulate the query
-SELECT 
-	ic.AffectedCompanyID As CompanyID,
+SELECT
+    ic.AffectedCompanyID AS CompanyID,
     de.EventID,
     dc.CategoryName AS DisruptionCategory,
     de.EventDate,
     de.EventRecoveryDate,
     ic.ImpactLevel
 FROM ImpactsCompany ic
-JOIN DisruptionEvent de 
+JOIN DisruptionEvent de
     ON ic.EventID = de.EventID
-JOIN DisruptionCategory dc 
+JOIN DisruptionCategory dc
     ON de.CategoryID = dc.CategoryID
-WHERE ic.AffectedCompanyID = 10 --make sure to change
+WHERE ic.AffectedCompanyID = 10
 ORDER BY de.EventDate DESC;
