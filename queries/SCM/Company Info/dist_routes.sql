9588f7ee229e2ab37566d5f7addd49234736b62b
eligonzalezz <elisamgoneli@gmail.com>
2025-11-14 16:33:11 -0500

    Create dist_routes.sql

---
+++ queries/SCM/Company Info/dist_routes.sql
@@ -0,0 +1,14 @@
+SELECT
+    d.CompanyName AS DistributorName,
+    fromC.CompanyName AS FromCompany,
+    toC.CompanyName AS ToCompany
+FROM Company d
+JOIN Distributor dist
+    ON dist.CompanyID = d.CompanyID
+JOIN OperatesLogistics ol
+    ON ol.DistributorID = d.CompanyID
+JOIN Company fromC
+    ON fromC.CompanyID = ol.FromCompanyID
+JOIN Company toC
+    ON toC.CompanyID = ol.ToCompanyID
+WHERE d.CompanyName = 'Davis PLC';
