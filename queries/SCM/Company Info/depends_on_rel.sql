5c27997d36397953c90b7678ae08eeecdadb968a
eligonzalezz <elisamgoneli@gmail.com>
2025-11-14 16:16:19 -0500

    Create depends_on_rel.sql

---
+++ queries/SCM/Company Info/depends_on_rel.sql
@@ -0,0 +1,14 @@
+SELECT 
+    c.CompanyName AS TargetCompany,
+    up.CompanyName AS UpstreamDependency,
+    down.CompanyName AS DownstreamDependency
+FROM Company c
+LEFT JOIN DependsOn d1 
+    ON d1.DownstreamCompanyID = c.CompanyID
+LEFT JOIN Company up 
+    ON up.CompanyID = d1.UpstreamCompanyID
+LEFT JOIN DependsOn d2
+    ON d2.UpstreamCompanyID = c.CompanyID
+LEFT JOIN Company down
+    ON down.CompanyID = d2.DownstreamCompanyID
+WHERE c.CompanyName = 'Chavez Ltd';
