717e80c36c8c2f6b7d68927dde153304a9ea0992
Mendoant <89116354+Mendoant@users.noreply.github.com>
2025-11-17 00:02:17 -0500

    Update depend_user_def_comp.sql
    Added user defined variable

--- queries/SCM/Company Info/depend_user_def_comp.sql
+++ queries/SCM/Company Info/depend_user_def_comp.sql
@@ -1,9 +1,11 @@
+SET @companyName = 'Chavez Ltd';
+
 SELECT
-    c.CompanyName AS TargetCompany,          -- the company you input
-    down.CompanyName AS DependentCompany    -- companies that depend on it
+    c.CompanyName AS TargetCompany,          
+    down.CompanyName AS DependentCompany    
 FROM Company c
 LEFT JOIN DependsOn d
     ON d.UpstreamCompanyID = c.CompanyID
 LEFT JOIN Company down
     ON down.CompanyID = d.DownstreamCompanyID
-WHERE c.CompanyName = 'Chavez Ltd';
+WHERE c.CompanyName = @companyName;
