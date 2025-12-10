2fd3fcf122d9ef9a754def8ef675bd6a734e9cbb
mateusztlisiecki <mlisiecki05@gmail.com>
2025-12-09 19:39:02 -0500

    Document source lines for SQL queries
    Added comments to SQL queries indicating their source lines in companies.php.

--- final_queries/critical_companies_queries.sql
+++ final_queries/critical_companies_queries.sql
@@ -1,17 +1,15 @@
---This file documents the SQL queries found in the critical_companies.php file. The lines where a query 
---is found will be commented prior to the exact code pulled from the file.
 
---Lines 18-19
+--  companies.php: lines 18-19
 SELECT c.CompanyID, c.CompanyName, c.Type, c.TierLevel, l.ContinentName, l.CountryName
         FROM Company c LEFT JOIN Location l ON c.LocationID = l.LocationID WHERE 1=1
 
---Line 32
+--  companies.php: lines 32
 SELECT COUNT(DISTINCT DownstreamCompanyID) as cnt FROM DependsOn WHERE UpstreamCompanyID = ?
 
---Lines 36-38
+--  companies.php: lines 36-38
 SELECT COUNT(DISTINCT de.EventID) as cnt FROM DisruptionEvent de 
                             JOIN ImpactsCompany ic ON de.EventID = ic.EventID 
                             WHERE ic.AffectedCompanyID = ? AND ic.ImpactLevel = 'High'
 
---Line 70
+--  companies.php: lines 70
 SELECT DISTINCT ContinentName FROM Location ORDER BY ContinentName
